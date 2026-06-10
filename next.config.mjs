db913a0d0de5bfa8cc7dfe41a48f46eedbec366f
RomeoLuna <lunaro2meo@gmail.com>
2026-06-10 16:20:23 -0600

    Fix headers dropping in middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverActions: {
-    allowedOrigins: ['faro-ilcbeer.netlify.app']
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['faro-ilcbeer.netlify.app']
+    }
   }
 };
 
-module.exports = nextConfig; // o export default nextConfig; según como lo tengas
+module.exports = nextConfig;
